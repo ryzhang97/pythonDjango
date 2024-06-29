b43383bf3495bce747d03d11eb2b171d017709a6
zhangruyi <1067852@greatwall.com.cn>
2024-06-29 12:29:03 +0800

    获取小程序opendi

--- wx/views.py
+++ wx/views.py
@@ -1,4 +1,3 @@
-import logging
 import requests
 
 from django.http import HttpResponse, HttpResponseServerError
@@ -10,29 +9,45 @@ from decouple import config
 APPID = config('APPID')
 APPSECRET = config('APPSECRET')
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-
+# 小程序登录
 class LoginView(APIView):
-    def get(self, request, format=None):
-        data = {'message': 'ok!', 'code': 200, 'data': 'login'}
-        return Response(data)
-
     def post(self, request, format=None):
-        print(request.data["code"])
-        data = {'message': 'ok!', 'code': 200, 'data': 'login'}
+        code = request.data["code"]
+        print(code)
+        res = jscode2session(code)
+        data = _res(res)
         return Response(data, status=status.HTTP_200_OK)
 
 
 class AccessTokenView(APIView):
     def get(self, request, format=None):
         res = wx_mini_token()
-        if res is None:
-            data = {'message': 'err!', 'code': 201, 'data': None}
-        else:
-            data = {'message': 'ok!', 'code': 200, 'data': res}
+        data = _res(res)
         return Response(data)
+
+
+def _res(res):
+    if res is None:
+        return {'message': 'err!', 'code': 201, 'data': None}
+    else:
+        return {'message': 'ok!', 'code': 200, 'data': res}
+
+
+def jscode2session(JSCODE):
+    # 设置你要请求的URL
+    url = 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code' % (
+        APPID, APPSECRET, JSCODE)
+    # 发送GET请求
+    response = requests.get(url)
+    # 检查请求是否成功
+    if response.status_code == 200:
+        # 处理成功的响应
+        data = response.json()  # 如果响应是JSON格式的话
+        return data
+    else:
+        # 处理请求失败的情况
+        return None
 
 
 def wx_mini_token():
