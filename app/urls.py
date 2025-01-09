e9eda237e01809d04abe08b892c9ff963529f0f8
zhangruyi <1067852@greatwall.com.cn>
2025-01-09 23:15:45 +0800

    view引入方式

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import include, path
+# from rest_framework import routers
+# from app.views import UserViewSet
 from app import views
 
+# router = routers.DefaultRouter()
+# router.register(r'users', UserViewSet)
+
 urlpatterns = [
+    # path('', include(router.urls)),
     path('custom', views.CustomView.as_view()),
     path('test', views.TestView.as_view()),
     path('login',views.LoginView.as_view()),
